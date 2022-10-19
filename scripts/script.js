7c87ab14ecb51ec6b8a1ebae83df8aab942df6d1
dikor77 <dakovyazin@gmail.com>
2022-10-19 13:30:06 +0600

    create card generator from array of images pathes

--- scripts/script.js
+++ scripts/script.js
@@ -1,3 +1,21 @@
+//cards list
+let cards = [
+    "images/img_lights_wide.jpg", 
+    "images/img_mountains_wide.jpg", 
+    "images/img_nature_wide.jpg"
+]
+
+//create cards
+cards.reverse().forEach((val, idx, arr) => createImageCard(
+    arr.length - idx, 
+    arr.length, 
+    val, 
+    "Slide " + (arr.length - idx) 
+))
+
+
+
+
 let slideIndex = 1;
 showSlides(slideIndex);
 
@@ -27,3 +45,24 @@ function showSlides(n) {
   dots[slideIndex-1].className += " active";
 }
 
+function createImageCard(slideNum, slidesTotal, imagePath, captionText){
+    let slidesContainer = document.getElementsByClassName("slideshow-container")[0];
+    let div_mySlide = document.createElement('div')
+    div_mySlide.setAttribute('class', "mySlides fade")
+    let div_numtext = document.createElement('div')
+    div_numtext.setAttribute('class', "numbertext")
+    div_numtext.innerText = `${slideNum} / ${slidesTotal}`
+    let div_image = document.createElement('img')
+    div_image.setAttribute('style', "width:100%")
+    div_image.setAttribute('src', imagePath)
+    let div_text = document.createElement('div')
+    div_text.setAttribute('class', "text")
+    div_text.innerText = captionText
+
+    div_mySlide.appendChild(div_numtext)
+    div_mySlide.appendChild(div_image)
+    div_mySlide.appendChild(div_text)
+
+    slidesContainer.insertBefore(div_mySlide, slidesContainer.firstChild)
+
+}
